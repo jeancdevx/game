4e4aa41d576ab3be4e3c3ca87973c65d9041122c
jcodev2 <jcode2006@gmail.com>
2024-11-07 03:12:36 -0500

    fix: Reduce projectile speed for improved gameplay balance

--- src/main/java/game/Config.java
+++ src/main/java/game/Config.java
@@ -6,7 +6,7 @@ public class Config {
   public static final int ENTITY_SIZE = 60;
   public static final int PLAYER_HEALTH = 5;
   public static final int ENEMY_HEALTH = 3;
-  public static final int PROJECTILE_SPEED = 10;
+  public static final int PROJECTILE_SPEED = 5;
   public static final int HEALTH_WIDTH = 250;
   public static final int HEALTH_HEIGHT = 25;
 }
